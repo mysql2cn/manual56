9190e819f5451e6aefdcbbdf0cad18030f2333cd
diguage <leejun119@gmail.com>
2013-09-15 20:28:33 +0800

    增加创建file的工具

---
+++ tools/create_file_by_filename.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python  
+# -*- coding: utf-8 -*-
+import os
+
+files = '../filename.txt'
+f = open(files, 'r+')
+line= f.readline()
+for line in f.readlines():
+    if line.startswith('#')or not line.split():
+        continue
+    print line,
+    cf = open(line.strip('\n'), 'w')
+    cf.close()
+    
+f.close()
